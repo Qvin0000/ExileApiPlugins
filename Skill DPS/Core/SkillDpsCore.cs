ab5b985c610d2e37b5f74efbb2f32ff2f69be0b0
Stridemann <stridemann@gmail.com>
2019-09-12 23:25:24 +0300

    Skill DPS fixed & refactored

--- Skill DPS/Core/SkillDpsCore.cs
+++ Skill DPS/Core/SkillDpsCore.cs
@@ -1,29 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using ExileCore;
+using ExileCore.PoEMemory;
+using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Enums;
 using SharpDX;
-using Skill_DPS.Skill_Data;
 using Vector2 = System.Numerics.Vector2;
 
 namespace Skill_DPS.Core
 {
-    public class Core : BaseSettingsPlugin<Settings>
+    public class SkillDpsCore : BaseSettingsPlugin<Settings>
     {
         public override void Render()
         {
             base.Render();
             var hoverUi = GameController.Game.IngameState.UIHoverTooltip.Tooltip;
 
-            var skills = SkillBar.CurrentSkills();
+            var ids = GameController.IngameState.ServerData.SkillBarIds;
+
+            if (ids == null || ids.Count == 0)
+            {
+                LogError("SkillBarIds is null or empty. Check offsets");
+                return;
+            }
+
+            var actorSkills = RemoteMemoryObject.pTheGame.IngameState.Data.LocalPlayer.GetComponent<Actor>().ActorSkills;
+
+            if (actorSkills == null || actorSkills.Count == 0)
+            {
+                LogError("ActorSkills is null or empty. Check offsets");
+                return;
+            }
+
+            var skills = CurrentSkills(ids, actorSkills);
+
+            if (skills == null || skills.Count == 0)
+            {
+                LogError("skills is null or empty");
+                return;
+            }
+
             foreach (var skill in skills)
             {
                 var box = skill.SkillElement.GetClientRect();
                 var newBox = new RectangleF(box.X, box.Y - 2, box.Width, -15);
 
                 decimal value;
-
-                if (hoverUi.GetClientRect().Intersects(newBox) && hoverUi.IsVisibleLocal) continue;
+                if (hoverUi != null && hoverUi.GetClientRect().Intersects(newBox) && hoverUi.IsVisibleLocal) continue;
 
                 if (skill.Skill.Stats.TryGetValue(GameStat.HundredTimesDamagePerSecond, out var val0))
                     value = val0 / (decimal) 100d;
@@ -37,9 +62,6 @@ namespace Skill_DPS.Core
                     value = val4 / (decimal) 100d;
                 else
                     continue;
-
-                //
-                // if (value <= 0) continue;
 
                 var pos = new Vector2(newBox.Center.X, newBox.Center.Y - Settings.FontSize / 2f);
                 Graphics.DrawText(ToKmb(Convert.ToDecimal(value)), pos, Settings.FontColor, Settings.FontSize, FontAlign.Center);
@@ -56,6 +78,41 @@ namespace Skill_DPS.Core
             if (num > 999) return num.ToString("0,.##K", CultureInfo.InvariantCulture);
 
             return num.ToString("0.#", CultureInfo.InvariantCulture);
+        }
+
+        public static List<Data> CurrentSkills(IList<ushort> ids, List<ActorSkill> actorSkills)
+        {
+            var returnSkills = new List<Data>();
+
+            for (var index = 0; index < ids.Count; index++)
+            {
+                var skillId = ids[index];
+
+                if (skillId == 0)
+                    continue;
+
+                foreach (var actorSkill in actorSkills)
+                {
+                    if (actorSkill.Id == skillId)
+                    {
+                        returnSkills.Add(new Data
+                        {
+                            Skill = actorSkill,
+                            SkillElement = RemoteMemoryObject.pTheGame.IngameState.IngameUi.SkillBar.Children[index]
+                        });
+
+                        break;
+                    }
+                }
+            }
+
+            return returnSkills;
+        }
+
+        public class Data
+        {
+            public ActorSkill Skill { get; set; }
+            public Element SkillElement { get; set; }
         }
     }
 }
