e030f72e8cfd6913160f482215ab43cb51cca492
TehCheat <thecheatuc@gmail.com>
2019-12-17 12:42:05 -0500

    Added BuffUtil

---
+++ BuffUtil/C.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace BuffUtil
+{
+    public static class C
+    {
+        public static class SteelSkin
+        {
+            public const string BuffName = "steelskin";
+            public const string Name = "QuicKGuard";
+            public const string InternalName = "steelskin";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(1);
+        }
+
+        public static class ImmortalCall
+        {
+            public const string BuffName = "mortal_call";
+            public const string Name = "ImmortalCall";
+            public const string InternalName = "mortal_call";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(1);
+        }
+
+        public static class MoltenShell
+        {
+            public const string BuffName = "molten_shell_shield";
+            public const string Name = "MoltenShell";
+            public const string InternalName = "molten_shell_barrier";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(1);
+        }
+
+        public static class PhaseRun
+        {
+            public const string BuffName = "new_phase_run";
+            public const string BuffName2 = "new_phase_run_damage";
+            public const string Name = "NewPhaseRun";
+            public const string InternalName = "NewPhaseRun";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(1);
+        }
+
+        public static class WitheringStep
+        {
+            public const string BuffName = "slither";
+            public const string Name = "Slither";
+            public const string InternalName = "slither";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(3);
+        }
+
+        public static class BloodRage
+        {
+            public const string BuffName = "blood_rage";
+            public const string Name = "BloodRage";
+            public const string InternalName = "blood_rage";
+            public static readonly TimeSpan TimeBetweenCasts = TimeSpan.FromSeconds(1);
+        }
+
+        public static class BladeFlurry
+        {
+            public const string BuffName = "charged_attack";
+        }
+
+        public static class ScourgeArrow
+        {
+            public const string BuffName = "virulent_arrow_counter";
+        }
+
+        public static class InfusedChanneling
+        {
+            public const string BuffName = "storm_barrier_support_damage";
+        }
+
+        public static class GracePeriod
+        {
+            public const string BuffName = "";
+        }
+    }
+}
